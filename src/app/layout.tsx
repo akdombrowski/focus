b5227df1f97a5d1a269dc1d21bd46a3505557e70
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-04-16 17:39:45 -0500

    init nextjs app with mui and yarn

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Anthony Dombrowski's Focus App",
+  description: "Focus more, focus better, focus more, focus better, focus more, focus better, ... what was I doing?",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
